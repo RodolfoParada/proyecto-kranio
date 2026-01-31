37530cc0038ef22779794e02f615c24b4927c610
Rodolfo Parada <rodolfo.parada.gonzalez>
2026-01-31 18:14:10 -0300

    feat: configurar archivo index.ts del backend para deploy del frontend

--- taskflow/backend/src/index.ts
+++ taskflow/backend/src/index.ts
@@ -1,4 +1,5 @@
 import app from './app';
+import cors from 'cors';
 
 const PORT = process.env.PORT || 4000;
 
@@ -8,3 +9,12 @@ if (process.env.NODE_ENV !== 'test') {
     console.log(`🚀 Server running on port ${PORT}`);
   });
 }
+
+const corsOptions = {
+  // Aquí pondremos la URL que Vercel le asigne en un momento
+  origin: ['https://proyecto-kranio-frontend.vercel.app', 'http://localhost:3000'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+};
+
+app.use(cors(corsOptions));
